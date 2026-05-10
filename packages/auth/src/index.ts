1368fd475a1455e364744792495f898b9254f09a
Theeraphat Jaingam <jain.theera@gmail.com>
2026-05-10 23:39:48 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@modticket/db";
+import * as schema from "@modticket/db/schema/auth";
+import { env } from "@modticket/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
